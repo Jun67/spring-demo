fcce1b30e236a45b8cbcb2ab91d49139565de7c1
Jun67 <244495794@qq.com>
2019-12-16 16:49:42 +0800

    Spring 入门示例

--- src/main/java/com/bailiban/day1/Client.java
+++ src/main/java/com/bailiban/day1/Client.java
@@ -13,8 +13,11 @@ public class Client {
     }
 
     public static void main(String[] args) {
+        // 通过xml文件，获取Spring容器上下文
         ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("applicationContext.xml");
+        // 使用Spring提供的getBean方法，从Spring容器中获取对象
         Client client = (Client) context.getBean("client");
+        // 我们并没有给Client对象设置userService属性，userService的创建是由Spring帮我们完成的
         client.userService.add(new User(1001, "Jim"));
     }
 }
